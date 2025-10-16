29b214c713152af6bcfc947783e9a69c0c74612a
beloff-ZA <benedict.eloff@gmail.com>
2025-10-17 00:30:05 +0200

    FIXXXX

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from "react";
-import AppRoutes from "@/Routes/AppRoutes";
+import AppRoutes from "@/routes/AppRoutes";
 
 const App: React.FC = () => {
   return <AppRoutes />;
