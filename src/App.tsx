31b787d3aff0f4a9d6ff8ad235bf84aba4a8a501
beloff-ZA <benedict.eloff@gmail.com>
2025-10-17 00:23:49 +0200

    FIX

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from "react";
-import AppRoutes from "@routes/AppRoutes";
+import AppRoutes from "@/Routes/AppRoutes";
 
 const App: React.FC = () => {
   return <AppRoutes />;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "@/routes/AppRoutes";
+import AppRoutes from "@/Routes/AppRoutes";
 import "@/styles/globals.css";
 import "@/styles/themes/default.css";
 
